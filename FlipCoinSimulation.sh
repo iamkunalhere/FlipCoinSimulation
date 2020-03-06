def6c33cdf4d317ee1bfca418009099b56682aa8
kunal jadhav <iamkunalhere@gmail.com>
2020-03-06 18:06:50 +0530

    Triplet combinations added

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -15,23 +15,6 @@ function singletRandom() {
 	fi
 }
 
-function doubletRandom() {
-	state=$((RANDOM%4))
-	if [[ $state -eq  1 ]]
-	then
-		echo "HH"
-	elif [[ $state -eq 2 ]]
-	then
-		echo "TT"
-	elif [[ $state -eq 3 ]]
-	then
-		echo "HT"
-	else
-		echo "TH"
-	fi
-}
-
-
 function percentage() {
 	temp=$1
 	percent=$(($temp*100/10))
@@ -47,10 +30,10 @@ for ((i=0; i<10; i++))
 do
 	z=$( singletRandom )
 	if [[ $z == H ]]
-	then
+	  then
 		((singletHeadCount++))
 		singlet[H]=$singletHeadCount
-	else
+	  else
 		((singletTailCount++))
 		singlet[T]=$singletTailCount
 	fi
@@ -65,23 +48,78 @@ doubletHeadCount=0
 doubletTailCount=0
 for ((j=0; j<10; j++))
 do
-   z=$( doubletRandom )
-   if [[ $z == HH ]]
+	y=$( singletRandom )
+   z=$( singletRandom )
+	res=$y$z
+   if [[ $res == HH ]]
    then
       ((doubletHeadCount++))
       doublet[HH]=$doubletHeadCount
-	elif [[ $z == TT ]]
+	elif [[ $res == TT ]]
 	then
 		((doubletTailCount++))
 		doublet[TT]=$doubletTailCount
-	elif [[ $z == HT ]]
+	elif [[ $res == HT ]]
 	then
 		((HeadThenTail++))
 		doublet[HT]=$HeadThenTail
-	elif [[ $z == TH ]]
+	elif [[ $res == TH ]]
 	then
       ((TailThenHead++))
       doublet[TH]=$TailThenHead
    fi
+done
+
+declare -A triplet
+
+triplet=( ["HHH"]=0 ["TTT"]=0 ["HHT"]=0 ["HTH"]=0 ["THH"]=0 ["THT"]=0 ["TTH"]=0 ["HTT"]=0)
+tripletHeadCount=0
+tripletTailCount=0
+HeadHeadTail=0
+HeadTailHead=0
+TailHeadHead=0
+TailHeadTail=0
+TailTailHead=0
+HeadTailTail=0
+for ((k=0; k<10; k++))
+do
+	x=$( singletRandom )
+	y=$( singletRandom )
+	z=$( singletRandom )
+	res=$x$y$z
+
+   if [[ $res == HHH ]]
+   then
+      ((tripletHeadCount++))
+      triplet[HHH]=$tripletHeadCount
+   elif [[ $res == TTT ]]
+   then
+      ((tripletTailCount++))
+      triplet[TTT]=$tripletTailCount
+   elif [[ $res == HHT ]]
+   then
+      ((HeadHeadTail++))
+      triplet[HHT]=$HeadHeadTail
+   elif [[ $res == HTH ]]
+   then
+      ((HeadTailHead++))
+      triplet[HTH]=$HeadTailHead
+   elif [[ $res == THH ]]
+	then
+		((TailHeadHead++))
+		triplet[THH]=$TailHeadHead
+	elif [[ $res == THT ]]
+	then
+		((TailHeadTail++))
+		triplet[THT]=$TailHeadTail
+	elif [[ $res == TTH ]]
+	then
+		((TailTailHead++))
+		triplet[TTH]=$TailTailHead
+	elif [[ $res == HTT ]]
+	then
+		((HeadTailTail++))
+		triplet=$HeadTailTail
+	fi
 done
 
