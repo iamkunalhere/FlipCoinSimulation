a86d2dc520a6e7df3ec6a82e764b09787dc9e740
kunal jadhav <iamkunalhere@gmail.com>
2020-03-06 17:13:02 +0530

    doublet dictionary

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,7 +5,7 @@
 #Author : Kunal Jadhav
 #Date : 6 March 2020
 
-function random() {
+function singletRandom() {
 	state=$((RANDOM%2))
 	if [[ $state -eq 1 ]]
 	then
@@ -14,6 +14,23 @@ function random() {
 		echo "T"
 	fi
 }
+
+function doubletRandom() {
+	state=$((RANDOM%4))
+	if [[ $state -eq  1 ]]
+	then
+		echo "HH"
+	elif [[ $state -eq 2 ]]
+	then
+		echo "TT"
+	elif [[ $state -eq 3 ]]
+	then
+		echo "HT"
+	else
+		echo "TH"
+	fi
+}
+
 
 function percentage() {
 	temp=$1
@@ -28,7 +45,7 @@ singletHeadCount=0
 singletTailCount=0
 for ((i=0; i<10; i++))
 do
-	z=$( random )
+	z=$( singletRandom )
 	if [[ $z == H ]]
 	then
 		((singletHeadCount++))
@@ -38,3 +55,33 @@ do
 		singlet[T]=$singletTailCount
 	fi
 done
+
+declare -A doublet
+
+doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0)
+HeadThenTail=0
+TailThenHead=0
+doubletHeadCount=0
+doubletTailCount=0
+for ((j=0; j<10; j++))
+do
+   z=$( doubletRandom )
+   if [[ $z == HH ]]
+   then
+      ((doubletHeadCount++))
+      doublet[HH]=$doubletHeadCount
+	elif [[ $z == TT ]]
+	then
+		((doubletTailCount++))
+		doublet[TT]=$doubletTailCount
+	elif [[ $z == HT ]]
+	then
+		((HeadThenTail++))
+		doublet[HT]=$HeadThenTail
+	elif [[ $z == TH ]]
+	then
+      ((TailThenHead++))
+      doublet[TH]=$TailThenHead
+   fi
+done
+
