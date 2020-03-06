5fc615da2cd2a733f25ceae1a85fcc07cf64035e
kunal jadhav <iamkunalhere@gmail.com>
2020-03-06 18:40:03 +0530

    Wining Combinations are displayed

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -5,13 +5,13 @@
 #Author : Kunal Jadhav
 #Date : 6 March 2020
 
-function singletRandom() {
+function random() {
 	state=$((RANDOM%2))
 	if [[ $state -eq 1 ]]
-	then
-		echo "H"
+		then
+			echo "H"
 	else
-		echo "T"
+			echo "T"
 	fi
 }
 
@@ -23,56 +23,74 @@ function percentage() {
 
 declare -A singlet
 
+function FlipOnce() {
+
 singlet=( ["H"]=0 ["T"]=0)
 singletHeadCount=0
 singletTailCount=0
+
 for ((i=0; i<10; i++))
 do
-	z=$( singletRandom )
+	z=$( random )
 	if [[ $z == H ]]
-	  then
+	 then
 		((singletHeadCount++))
 		singlet[H]=$singletHeadCount
-	  else
+	else
 		((singletTailCount++))
 		singlet[T]=$singletTailCount
 	fi
 done
 
+echo "Ocurrance of combinations ${singlet[@]}"
+echo "Wining combinations are ${!singlet[@]}"
+
+}
+
 declare -A doublet
+
+function FlipTwice() {
 
 doublet=( ["HH"]=0 ["TT"]=0 ["HT"]=0 ["TH"]=0)
 HeadThenTail=0
 TailThenHead=0
 doubletHeadCount=0
 doubletTailCount=0
+
 for ((j=0; j<10; j++))
 do
-	y=$( singletRandom )
-   z=$( singletRandom )
+	y=$( random )
+   z=$( random )
 	res=$y$z
    if [[ $res == HH ]]
-   then
-      ((doubletHeadCount++))
-      doublet[HH]=$doubletHeadCount
+   	then
+      	((doubletHeadCount++))
+      	doublet[HH]=$doubletHeadCount
 	elif [[ $res == TT ]]
-	then
-		((doubletTailCount++))
-		doublet[TT]=$doubletTailCount
+		then
+			((doubletTailCount++))
+			doublet[TT]=$doubletTailCount
 	elif [[ $res == HT ]]
-	then
-		((HeadThenTail++))
-		doublet[HT]=$HeadThenTail
+		then
+			((HeadThenTail++))
+			doublet[HT]=$HeadThenTail
 	elif [[ $res == TH ]]
-	then
-      ((TailThenHead++))
-      doublet[TH]=$TailThenHead
+		then
+      	((TailThenHead++))
+      	doublet[TH]=$TailThenHead
    fi
 done
+echo "Ocurrance of combinations ${doublet[@]}"
+echo "Wining combinations are ${!doublet[@]}"
+
+}
 
 declare -A triplet
 
+function FlipThrice() {
+
 triplet=( ["HHH"]=0 ["TTT"]=0 ["HHT"]=0 ["HTH"]=0 ["THH"]=0 ["THT"]=0 ["TTH"]=0 ["HTT"]=0)
+
 tripletHeadCount=0
 tripletTailCount=0
 HeadHeadTail=0
@@ -81,45 +99,67 @@ TailHeadHead=0
 TailHeadTail=0
 TailTailHead=0
 HeadTailTail=0
+
 for ((k=0; k<10; k++))
 do
-	x=$( singletRandom )
-	y=$( singletRandom )
-	z=$( singletRandom )
+	x=$( random )
+	y=$( random )
+	z=$( random )
 	res=$x$y$z
 
    if [[ $res == HHH ]]
-   then
-      ((tripletHeadCount++))
-      triplet[HHH]=$tripletHeadCount
-   elif [[ $res == TTT ]]
-   then
-      ((tripletTailCount++))
-      triplet[TTT]=$tripletTailCount
-   elif [[ $res == HHT ]]
-   then
-      ((HeadHeadTail++))
-      triplet[HHT]=$HeadHeadTail
-   elif [[ $res == HTH ]]
-   then
-      ((HeadTailHead++))
-      triplet[HTH]=$HeadTailHead
-   elif [[ $res == THH ]]
-	then
-		((TailHeadHead++))
-		triplet[THH]=$TailHeadHead
-	elif [[ $res == THT ]]
-	then
-		((TailHeadTail++))
-		triplet[THT]=$TailHeadTail
-	elif [[ $res == TTH ]]
-	then
-		((TailTailHead++))
-		triplet[TTH]=$TailTailHead
-	elif [[ $res == HTT ]]
-	then
-		((HeadTailTail++))
-		triplet=$HeadTailTail
+   	then
+      	((tripletHeadCount++))
+      	triplet[HHH]=$tripletHeadCount
+   	elif [[ $res == TTT ]]
+   		then
+      		((tripletTailCount++))
+      		triplet[TTT]=$tripletTailCount
+   	elif [[ $res == HHT ]]
+   		then
+      		((HeadHeadTail++))
+      		triplet[HHT]=$HeadHeadTail
+   	elif [[ $res == HTH ]]
+   		then
+      	((HeadTailHead++))
+      		triplet[HTH]=$HeadTailHead
+   	elif [[ $res == THH ]]
+			then
+				((TailHeadHead++))
+				triplet[THH]=$TailHeadHead
+		elif [[ $res == THT ]]
+			then
+				((TailHeadTail++))
+				triplet[THT]=$TailHeadTail
+		elif [[ $res == TTH ]]
+			then
+				((TailTailHead++))
+				triplet[TTH]=$TailTailHead
+		elif [[ $res == HTT ]]
+			then
+				((HeadTailTail++))
+				triplet=$HeadTailTail
 	fi
 done
+
+echo "Ocurrance of combinations ${triplet[@]}"
+echo "Wining combinations are ${!triplet[@]}"
+
+}
+
+echo "You can only flip coin 1,2 or 3 times"
+read -p "How many times you want to flip a coin: " times
+
+if [[ $times -eq 1 ]]
+	then
+		FlipOnce
+	elif [[ $times -eq 2 ]]
+		then
+			FlipTwice
+	elif [[ $times -eq 3 ]]
+		then
+			FlipThrice
+	else
+		echo "Wrong choice"
+fi
 
